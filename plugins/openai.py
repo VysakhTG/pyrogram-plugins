325964850acb09ab1bd9a457470dad9084a85c66
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2023-03-08 15:45:37 +0530

    `New [ openai ]`

---
+++ plugins/openai.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) @VysakhTG
+
+from pyrogram import Client, filters
+from utils import ask_ai
+
+@Client.on_message(filters.command('openai'))
+async def openai_ask(client, message):
+    if len(message.command) == 1:
+       return await message.reply_text("Give an input!")
+    m = await message.reply_text("👀")
+    await ask_ai(client, m, message)
